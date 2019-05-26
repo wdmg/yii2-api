796eea48a8cf16232f11d7cc16787c0c778d1253
alex-wdmg <wdmg.com.ua@gmail.com>
2019-05-26 03:49:45 +0300

    Version 0.0.2

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,9 @@
 Changelog
 =========
 
+## 0.0.2 (2019-05-26)
+ * Added first (test) API for Module::Users
+ 
 ## 0.0.1 (2019-05-25)
  * Added composer package
  * Added bootstrap, base module
--- Module.php
+++ Module.php
@@ -6,7 +6,7 @@ namespace wdmg\api;
  * Yii2 API
  *
  * @category        Module
- * @version         0.0.1
+ * @version         0.0.2
  * @author          Alexsander Vyshnyvetskyy <alex.vyshnyvetskyy@gmail.com>
  * @link            https://github.com/wdmg/yii2-api
  * @copyright       Copyright (c) 2019 W.D.M.Group, Ukraine
@@ -44,7 +44,7 @@ class Module extends \yii\base\Module
     /**
      * @var string the module version
      */
-    private $version = "0.0.3";
+    private $version = "0.0.2";
 
     /**
      * @var integer, priority of initialization
